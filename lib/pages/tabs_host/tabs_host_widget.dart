f0d389ec16ff2ea77c3bdb19a54c1688eaafd7f9
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-09 18:59:06 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/tabs_host/tabs_host_widget.dart
+++ lib/pages/tabs_host/tabs_host_widget.dart
@@ -250,11 +250,6 @@ class _TabsHostWidgetState extends State<TabsHostWidget> {
                       },
                       onUserTap: () async {
                         HapticFeedback.lightImpact();
-                        await _model.tabsPagerController?.animateToPage(
-                          4,
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.ease,
-                        );
                         FFAppState().activeTabIndex = 4;
                         safeSetState(() {});
                         Navigator.pop(context);
