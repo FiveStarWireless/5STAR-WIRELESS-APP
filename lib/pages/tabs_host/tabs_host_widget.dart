84e4dbd258e21b3a99d27828d2f79e9b7faa1fbf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-16 17:23:58 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/tabs_host/tabs_host_widget.dart
+++ lib/pages/tabs_host/tabs_host_widget.dart
@@ -1,3 +1,4 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_web_view.dart';
@@ -125,71 +126,21 @@ class _TabsHostWidgetState extends State<TabsHostWidget> {
                           },
                           scrollDirection: Axis.horizontal,
                           children: [
-                            RefreshIndicator(
-                              onRefresh: () async {
-                                HapticFeedback.lightImpact();
-                                FFAppState().reloadTick =
-                                    FFAppState().reloadTick + 1;
-                                safeSetState(() {});
-                                await Future.delayed(
-                                  Duration(
-                                    milliseconds: 500,
-                                  ),
-                                );
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                      'Refreshed!',
-                                      style: TextStyle(
-                                        color: FlutterFlowTheme.of(context)
-                                            .primaryText,
-                                      ),
-                                    ),
-                                    duration: Duration(milliseconds: 4000),
-                                    backgroundColor:
-                                        FlutterFlowTheme.of(context).primary,
-                                  ),
-                                );
-                              },
-                              child: ListView(
-                                padding: EdgeInsets.zero,
-                                scrollDirection: Axis.vertical,
-                                children: [
-                                  Container(
-                                    width:
-                                        MediaQuery.sizeOf(context).width * 1.0,
-                                    height:
-                                        MediaQuery.sizeOf(context).height * 1.0,
-                                    decoration: BoxDecoration(
-                                      color: FlutterFlowTheme.of(context)
-                                          .secondaryBackground,
-                                    ),
-                                    child: Container(
-                                      width: MediaQuery.sizeOf(context).width *
-                                          1.0,
-                                      height:
-                                          MediaQuery.sizeOf(context).height *
-                                              1.0,
-                                      child: Stack(
-                                        children: [
-                                          FlutterFlowWebView(
-                                            content:
-                                                'https://5star-wireless.com/?t=\${FFAppState().reloadTick.toString()}',
-                                            bypass: true,
-                                            width: MediaQuery.sizeOf(context)
-                                                    .width *
-                                                1.0,
-                                            height: MediaQuery.sizeOf(context)
-                                                    .height *
-                                                1.0,
-                                            verticalScroll: true,
-                                            horizontalScroll: true,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                            Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                color: FlutterFlowTheme.of(context)
+                                    .secondaryBackground,
+                              ),
+                              child: FlutterFlowWebView(
+                                content:
+                                    'https://5star-wireless.com/?t=\${FFAppState().reloadTick.toString()}',
+                                bypass: true,
+                                width: MediaQuery.sizeOf(context).width * 1.0,
+                                height: MediaQuery.sizeOf(context).height * 1.0,
+                                verticalScroll: true,
+                                horizontalScroll: true,
                               ),
                             ),
                             Container(
@@ -325,6 +276,55 @@ class _TabsHostWidgetState extends State<TabsHostWidget> {
                     ),
                   ),
                 ],
+              ),
+              Align(
+                alignment: AlignmentDirectional(1.0, -1.0),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 15.0, 0.0),
+                  child: Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      shape: BoxShape.circle,
+                    ),
+                    child: FlutterFlowIconButton(
+                      borderRadius: 8.0,
+                      buttonSize: 40.0,
+                      fillColor: FFAppState().isDarkMode
+                          ? Color(0xFF0F1115)
+                          : Colors.white,
+                      icon: Icon(
+                        Icons.refresh_rounded,
+                        color: Color(0xFF07BCFD),
+                        size: 24.0,
+                      ),
+                      onPressed: () async {
+                        HapticFeedback.lightImpact();
+                        FFAppState().reloadTick = FFAppState().reloadTick + 1;
+                        safeSetState(() {});
+                        await Future.delayed(
+                          Duration(
+                            milliseconds: 500,
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              'Refreshed!',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            duration: Duration(milliseconds: 4000),
+                            backgroundColor:
+                                FlutterFlowTheme.of(context).secondary,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
